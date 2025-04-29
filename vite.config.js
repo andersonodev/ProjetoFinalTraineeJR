87b51764128481031e23066593df7a7d0763b913
andersonodev <andersonodev@gmail.com>
2025-04-29 15:54:56 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'; // <- Corrigido!
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/ProjetoFinalTraineeJR/',  // <-- Nome do repositório aqui
+  base: '/ProjetoFinalTraineeJR/',  // <--- ESSENCIAL! Nome EXATO do repositório
   plugins: [react()],
-});
+})
